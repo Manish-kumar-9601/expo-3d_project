254d8a448c9f026225265e37423b060ffb7c186a
Manish kumar <134037752+Manish-kumar-9601@users.noreply.github.com>
2024-12-27 14:08:38 +0530

    add character animations and integrate with Zustand store; preload animation assets
    Signed-off-by: Manish kumar <134037752+Manish-kumar-9601@users.noreply.github.com>

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,10 +1,16 @@
-﻿import React from 'react'
+﻿import React, { Suspense, useRef } from 'react'
 import { Character } from './Character'
 
 
 export const Player = () =>
 {
+    const groupRef = useRef();
+
     return (
+        <group ref={groupRef} >
+         <Suspense  fallback={null}>
         <Character />
+        </Suspense>
+        </group>
     )
 }
