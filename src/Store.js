b2449e8057263ad8a9e89c628b926676f4768a70
Manish kumar <134037752+Manish-kumar-9601@users.noreply.github.com>
2024-12-28 11:28:01 +0530

    feat:improve state management structure an details of states
    Signed-off-by: Manish kumar <134037752+Manish-kumar-9601@users.noreply.github.com>

--- src/Store.js
+++ src/Store.js
@@ -1,10 +1,17 @@
-﻿import { AnimationMixer } from "three";
-import { create } from "zustand";
+﻿
+import { create } from 'zustand';
+import { AnimationMixer } from 'three';
+import { useRef } from 'react';
 
-export const useStore=create(()=>(
-    {
-        actions:{},
-        mixer:new AnimationMixer(),
-        groundObject:{}
-    }
-))
+export const useStore = create(() => ({
+    // Define actions or methods to manipulate the state
+    actions: {},
+    // Create an instance of AnimationMixer for managing animations
+    mixer: new AnimationMixer(),
+
+    // Object to store information about the ground object
+    groundObject: {},
+
+    // Reference to a DOM element or 3D object
+
+}));
